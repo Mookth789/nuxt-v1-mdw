302fda4378553613bb24c22068ffa4215d6ad94d
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-11 00:29:56 +0700

    4 files modified, 1 added

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,15 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { 
+        rel: 'icon', 
+        type: 'image/x-icon', 
+        href: '/favicon.ico' 
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/@nutui/nutui-jdl@1.0.3/dist/nutui.css',
+      }
     ]
   },
 
